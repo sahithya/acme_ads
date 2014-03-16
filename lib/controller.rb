d5adaf363c615bc63cf2b4f869eecae2f3ae3085
sahithya <sahithya.wintrich@leandogsoftware.com>
2014-03-16 12:41:12 -0400

    Added test to add a newspaper to the controller.

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,15 @@
 class Controller
 
+	def initialize
+		@newspapers = []
+	end
+
 	def list_newspapers
-		[]
+		@newspapers
+	end
+
+	def add_newspaper(newspaper)
+		@newspapers.push(newspaper)
 	end
 
 end
--- spec/lib/controller_spec.rb
+++ spec/lib/controller_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'controller'
+require 'newspaper'
 
 describe Controller do
 
@@ -7,6 +8,13 @@ describe Controller do
 
 	it "should be empty when no newspapers have been added" do
 		controller.list_newspapers.should be_empty
+	end
+
+	it "should add a newspaper to the list" do
+		newspaper = Newspaper.new
+		controller.add_newspaper(newspaper)
+		controller.list_newspapers.size.should == 1
+		controller.list_newspapers.first.should == newspaper
 	end
 
 end
